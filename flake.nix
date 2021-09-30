ae0a4ea237367c091e6ceb02964b25de1291978e
Alexander Serowy <serowy@hotmail.com>
2021-09-30 14:04:43 +0200

    workaround for dots flake with git submodule
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -4,13 +4,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    dots = {
+    /* dots = {
       url = "github:aserowy/dots";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    }; */
+    home-manager = {
+      url = "github:nix-community/home-manager/release-21.05";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
-  outputs = { dots, nixpkgs, nixpkgs-unstable, ... }: {
+  outputs = { home-manager, nixpkgs, nixpkgs-unstable, ... }: {
     nixosConfigurations = {
       workstation = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -32,7 +36,20 @@
                 ];
             }
           )
-          dots.nixosModules."serowy@desktop-nixos"
+          home-manager.nixosModule {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+
+              users.serowy = import ./home/environments/desktop.nix;
+            };
+          }
+          /* ({config, lib, pkgs, utils,...}: dots.nixosModules."serowy@desktop-nixos" {
+            config = config;
+            lib = lib;
+            pkgs = pkgs;
+            utils = utils;
+          }) */
         ];
       };
     };
