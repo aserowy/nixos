dd115fc5c75773b3175a7770986721c5ac3dbc2b
Alexander Serowy <serowy@hotmail.com>
2021-09-30 14:33:44 +0200

    switched to gnome
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -2,40 +2,22 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     /* dots = {
       url = "github:aserowy/dots";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     }; */
-    home-manager = {
-      url = "github:nix-community/home-manager/release-21.05";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { home-manager, nixpkgs, nixpkgs-unstable, ... }: {
+  outputs = { home-manager, nixpkgs, ... }: {
     nixosConfigurations = {
       workstation = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ({ config, pkgs, ... }:
-            let
-              overlay-unstable = final: prev: {
-                unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
-              };
-            in
-            {
-              nixpkgs.overlays = [ overlay-unstable ];
-
-              imports =
-                [
-                  ./hardware-configuration.nix
-                  ./configuration.nix
-                  ./packages.nix
-                ];
-            }
-          )
+          ./hardware-configuration.nix
+          ./configuration.nix
+          ./packages.nix
           home-manager.nixosModule {
             home-manager = {
               useGlobalPkgs = true;
