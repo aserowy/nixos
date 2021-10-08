8b4a26c67f8f6794f95c3355603160dfcdbcb98d
aserowy <serowy@hotmail.com>
2021-10-08 06:35:44 +0200

    added rnix lsp to dev shell
    Signed-off-by: aserowy <serowy@hotmail.com>

--- shell.nix
+++ shell.nix
@@ -2,18 +2,8 @@ with import <nixpkgs> { };
 mkShell rec {
   name = "nix";
   buildInputs = [
-    pkgs.nixpkgs-fmt
     pkgs.nodePackages.markdownlint-cli
     pkgs.nodePackages.prettier
-    pkgs.stylua
-    pkgs.sumneko-lua-language-server
+    pkgs.rnix-lsp
   ];
-  shellHook = ''
-    # format
-    alias fmt="prettier --write README.md && nixpkgs-fmt ."
-
-    # update wezterm config
-    alias wu="cp ./programs/wezterm/wezterm.lua /mnt/c/Users/serow/.wezterm.lua"
-    alias wuy="cp ./programs/wezterm/wezterm.lua /mnt/c/Users/serowy/.wezterm.lua"
-  '';
 }
