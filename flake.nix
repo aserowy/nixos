028f54caa1b0ad19cd93ad7139710a983bf1ef6a
aserowy <serowy@hotmail.com>
2021-10-15 09:48:10 +0200

    removed wayland overlays because of dunst double free
    Signed-off-by: aserowy <serowy@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
   inputs = {
     home.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+#    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
     nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { home, nixpkgs, nixpkgs-wayland, nur, ... }: {
+  #outputs = { home, nixpkgs, nixpkgs-wayland, nur, ... }: {
+  outputs = { home, nixpkgs, nur, ... }: {
     nixosConfigurations = {
       desktop-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -16,7 +17,7 @@
           ./configuration.nix
           {
             nixpkgs.overlays = [
-              nixpkgs-wayland.overlay
+#              nixpkgs-wayland.overlay
               nur.overlay
 
               (import ./home/pkgs)
@@ -36,6 +37,3 @@
     };
   };
 }
-
-
-
