4d30450c82336ededce1c3154b066006c872d677
aserowy <serowy@hotmail.com>
2021-11-04 06:13:54 +0100

    picom blur and borders
    Signed-off-by: aserowy <serowy@hotmail.com>

--- shell/i3/picom.nix
+++ shell/i3/picom.nix
@@ -4,14 +4,18 @@
     enable = true;
     activeOpacity = 0.95;
     backend = "glx";
+    fade = true;
     inactiveOpacity = 0.8;
     settings = {
       blur = {
-        method = "gaussian";
-        size = 10;
-        deviation = 5.0;
+        method = "dual_kawase";
+        kern = "3x3box";
+      };
+      corner = {
+        radius = 6.0;
       };
     };
     shadow = true;
+    vSync = true;
   };
 }
